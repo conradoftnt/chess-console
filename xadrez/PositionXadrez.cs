08b05ac82a63bed141af77a9fa3f95b4e32e9b31
Conrado Fortunato <conradofortunato246@gmail.com>
2023-07-15 14:10:01 -0300

    feat: creating class PositionXadrez

---
+++ xadrez/PositionXadrez.cs
@@ -0,0 +1,26 @@
+﻿using board;
+
+namespace xadrez
+{
+    class PositionXadrez
+    {
+        public int line { get; set; }
+        public char column { get; set; }
+
+        public PositionXadrez(int line, char column)
+        {
+            this.line = line;
+            this.column = column;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - line, column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return $"{column}{line}";
+        }
+    }
+}
